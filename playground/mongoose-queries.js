4ee843a1cd19d6a10ea48ae8708bd92de756620b
Gauri Bokare <gauripeshkar@gmail.com>
2018-04-27 16:08:39 +0530

    2nd Setup app for Heroku

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,47 +1,40 @@
-const {ObjectId} = require('mongodb');
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/Todo');
 const {User} = require('./../server/models/Users');
 
-//var id = '6adf178176d9e618017a59e8';
-
-// if(!ObjectId.isValid(id)){
-//     console.log('ID not valid');
-// } 
+// var id = '57bf38394b39c93d2a557e9811';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
 
 // Todo.find({
-//     _id: id
-// }).then((todos) =>{
-//     console.log('Todos' ,todos); 
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
 // });
-
+//
 // Todo.findOne({
-//     _id: id
-// }).then((todo) =>{
-//     console.log('Todo',todo);
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
 // });
 
-// Todo.findById(id).then((todo) =>{
-//     if(!todo){
-//         return console.log('Id not found');
-//     }
-//     console.log('Todo By Id',todo);
-// }).catch((e) => conosle.log(e));
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
 
+User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
 
-
-User.findById('5ad71850ba3b7ea217fb64e6').then((user) =>{
-    if(!user){
-        return console.log('User not found');
-    }
-    console.log('User found', user );
-},(e) =>{
-    console.log((e));
-
-})
-
-
-
-
-
-
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
--- server/server.js
+++ server/server.js
@@ -1,62 +1,56 @@
-// library imports
 var express = require('express');
-var bodyParser= require('body-parser');
-var ObjectID = require('mongodb');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-// local imports
-var {mongoose} = require('./db/mongoose') ;
-var {Todo} =require('./models/Todo');
-var {Users} =require('./models/Users');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/Users');
 
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-//creating a new todo
-app.post('/todos',(req,res) =>{
-    var todo = new Todo({
-        text:req.body.text,
-        completed:req.body.completed
-    });
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc) =>{
-        res.send(doc);
-    },(e) =>{
-        res.status(400).send(e);
-    });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-
-app.get('/todos',(req,res) => {
-    Todo.find().then((todos) =>{
-        res.send({todos});
-    },(e) =>{
-        res.status(400).send(e);
-    });
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// Get request /todos
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-app.get('/todos/:id',(req,res) =>{
-    var id=req.params.id;
-    if(!ObjectID.isValid(id)){
-      return  res.status(404).send();
-    } 
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-Todo.FindById(id).then((todo) =>{
-    if(!todo){
-       return res.status(404).send(); 
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
+
     res.send({todo});
-}).catch((e) =>{
+  }).catch((e) => {
     res.status(400).send();
+  });
 });
 
-
-});
-app.listen(port,() =>{
-    console.log(`Started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
